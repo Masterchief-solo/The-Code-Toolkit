6548192cd3b595fa461ad9e7bed50b918e9c0f38
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-09-24 15:11:13 -0500

    Create Challenge34.R

---
+++ R/Challenge34.R
@@ -0,0 +1,8 @@
+# starting weights vector
+starting_weights <- c(145,122,127,245,,362,152)
+
+# ending weights vector
+ending_weights <- c(125,128,117,205,300,173)
+
+# subtract starting_weight from ending_weight
+ending_weights-starting_weights
