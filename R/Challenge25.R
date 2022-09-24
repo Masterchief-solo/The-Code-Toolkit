ea2c66dbfd8b80fc8a4e4fc839fda5e6b13fb921
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-09-24 14:40:56 -0500

    Create Challenge25.R

---
+++ R/Challenge25.R
@@ -0,0 +1,8 @@
+# lottery numbers vector
+lottery_nums <-c(15, 22, 27, 45, 62, 5)
+
+# header row
+names(lottery_nums) <-c("Num1", "Num2", "Num3", "Num4", "Num5", "Powerball")
+
+# print lottery_nums
+print(lottery_nums)
