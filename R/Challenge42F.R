f6f45f5f1571a50965cdb5cb66dfc77bb6838785
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-09-25 16:35:58 -0500

    Create Challenge42F.R

---
+++ R/Challenge42F.R
@@ -0,0 +1,11 @@
+# load the library dataset
+library(datasets)
+
+# load the BJsales datasets
+data(BJsales)
+
+# run help on the BJsales dataset
+help(BJsales)
+
+# run a summary on the BJsales dataset
+summary(BJsales)
