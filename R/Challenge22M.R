73603d437e5774bcd8fb093c8e42d2c0ac81a4c7
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-09-25 10:20:25 -0500

    Create Challenge22M.R

---
+++ R/Challenge22M.R
@@ -0,0 +1,7 @@
+#create matA and print it
+matA = matrix( 
+  c(5, 2, 1, 4, 7, 6), # the data elements 
+  nrow=2,              # number of rows 
+  ncol=3,              # number of columns 
+  byrow = FALSE)        # fill matrix by columns 
+matA  
