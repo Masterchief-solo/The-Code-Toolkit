78863de2de9f7d7773dc32a043f044507995944c
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-09-25 16:08:00 -0500

    Create Challenge22F.R

---
+++ R/Challenge22F.R
@@ -0,0 +1,8 @@
+# create a factor
+HighestEducationCompleted <- factor(c("MiddleSchool","HighSchool","Undergrad","Grad","Doctorate","Other"))
+
+# print the levels
+levels(HighestEducationCompleted)
+
+# print the number of levels
+nlevels(HighestEducationCompleted)
