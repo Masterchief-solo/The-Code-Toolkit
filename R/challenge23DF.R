4ec9b653b2fc03b28a2462d2c32ea84ac64b3358
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-10-02 20:12:17 -0500

    Create challenge23DF.R

---
+++ R/challenge23DF.R
@@ -0,0 +1,13 @@
+# create vectors
+vplayer = c("Player1", "Player2", "Player3", "Player4", "Player5") 
+vr1 = c(68, 68, 64, 66, 70) 
+vr2 = c(65, 69, 70, 62, 70) 
+vr3 = c(66, 73, 64, 72, 64) 
+vr4 = c(69, 68, 71, 70, 78) 
+vtot = c(268, 278, 269, 270, 282)
+
+# create the data frame
+dfgolf = data.frame(vplayer, vr1, vr2, vr3, vr4, vtot)
+
+# print the data frame
+dfgolf
