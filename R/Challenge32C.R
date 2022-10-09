7b93ab9ed0e3ebc2db183907d3219c59ee46cf3c
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-10-08 21:58:08 -0500

    Update Challenge32C.R

--- R/Challenge32C.R
+++ R/Challenge32C.R
@@ -6,7 +6,7 @@ while (die1<1.25) { #check condition of control variable
   
   vd1d2<-c((die1+.25))#create vector
   
-  prntvd1d2<-c(die1) #create vector die1, die1+die2 and total of all pairs
+  prntvd1d2<-c(die1) #create vector die1
   
   print(prntvd1d2) #print vector as you process each die1
   
