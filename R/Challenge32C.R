2daa80df5f20dacf3837d6ebf71b9b5b281c8463
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-10-08 20:03:08 -0500

    Create Challenge32C.R

---
+++ R/Challenge32C.R
@@ -0,0 +1,15 @@
+#Control Flow -- WHILE LOOP
+
+die1<- -1 #set control variable
+
+while (die1<1.25) { #check condition of control variable
+  
+  vd1d2<-c((die1+.25))#create vector
+  
+  prntvd1d2<-c(die1) #create vector die1, die1+die2 and total of all pairs
+  
+  print(prntvd1d2) #print vector as you process each die1
+  
+  die1<-die1+.25 #increment control variable
+  
+}
