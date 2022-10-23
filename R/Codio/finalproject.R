b9c800f5fb448b3553c6d70efa94b5380e303948
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-10-23 10:14:24 -0500

    Update finalproject.R

--- R/Codio/finalproject.R
+++ R/Codio/finalproject.R
@@ -27,3 +27,25 @@ gbrAvgTemp <- mean(gbr$'Temperature (C)')          # average temperature
 gbrMinRain <- min(gbr$'Rainfall (mm)')             # minimum rainfall
 gbrMaxRain <- max(gbr$'Rainfall (mm)')             # maximum rainfall
 gbrAvgRain <- mean(gbr$'Rainfall (mm)')            # average rainfall
+
+# Working with data structures
+# Building a matrix
+usaM <- rbind(usaMinTemp, usaMaxTemp, usaAvgTemp, usaMinRain, usaMaxRain, usaAvgRain) # create vector
+colnames(usaM) <- c("USA")                                                            # label your column
+rownames(usaM) <- c("Min Temperature", "Max Temperature", "Mean Temperature",
+"Min Rainfall", "Max Rainfall", "Mean Rainfall")                                      # label your rows
+usaM                                                                                  # print your matrix
+
+# Building a vector
+gbrV <- c(gbrMinTemp, gbrMaxTemp, gbrAvgTemp, gbrMinRain, gbrMaxRain, gbrAvgRain)     # create a vector from variables
+gbrV                                                                                  # print vector
+
+# Combining data structures into a single matrix
+weatherM <- cbind(usaM, gbrV)                                                         # combine matrix and vector into single matrix
+colnames(weatherM) <- c("USA", "GBR")                                                 # label columns of single matrix
+weatherM                                                                              # print combined matrix
+
+# Create a dataframe
+df <- data.frame(weatherM, usaM-gbrV)                                                 # create dataframe with difference
+colnames(df) <- c("USA", "GBR", "USA vs GBR")                                         # label columns of dataframe
+df                                                                                    # print dataframe
